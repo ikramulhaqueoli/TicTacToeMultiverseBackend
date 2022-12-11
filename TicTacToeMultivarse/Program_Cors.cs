43817b0b907c0b4fdc550efe014b786f8cf2c69f
Ikramul Haque Oli <ikramul.haque@agoda.com>
2022-12-11 09:19:00 +0700

    Refactor

---
+++ TicTacToeMultivarse/Program_Cors.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+internal partial class Program
+{
+	private static void AddCors(WebApplicationBuilder builder, WebApplication app)
+	{
+        // Default Policy
+        builder.Services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(
+                builder =>
+                {
+                    builder.WithOrigins(
+                        "http://localhost:4200",
+                        "http://localhost:8080",
+                        "https://localhost:4200",
+                        "https://localhost:443",
+                        "https://dhiman-angular-game.netlify.app")
+                                        .AllowAnyHeader()
+                                        .AllowAnyMethod();
+                });
+        });
+
+        app.UseCors();
+    }
+}
